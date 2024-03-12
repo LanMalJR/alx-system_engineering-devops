21b528aa3225898911abb34a81cda385dca8ccc7
LanMalJR <lanmaljrlm>
2024-03-12 12:41:06 -0700

    task6

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,2 +1,14 @@
-#!/usr/bin/env ruby
-puts ARGV[0].scan(/^\d{10}$/).join
+#!/bin/bash
+
+# Check if the argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <phone_number>"
+    exit 1
+fi
+
+# Check if the phone number matches the regular expression
+if [[ $1 =~ ^[[:space:]]*([0-9]{10})[[:space:]]*$ ]]; then
+    echo "${BASH_REMATCH[1]}"
+else
+    echo "Invalid phone number"
+fi
