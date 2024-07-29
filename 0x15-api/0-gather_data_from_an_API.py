dea585a8275b30be9eb471d066f8c550d6b5553e
LanMalJR <lanmaljrlm@gmail.com>
2024-07-29 20:58:37 +0200

    updated

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,32 +1,39 @@
-#!/usr/bin/python3
-'''
-This script uses a REST API to retrieve information
-about an employees TODO list progress
-'''
+import requests
 
-from requests import get
-from sys import argv
+def get_todo_list_progress(employee_id):
+    """
+    Retrieves the TODO list progress for a given employee ID.
 
-if __name__ == '__main__':
-    id = argv[1]
-    todos_res = get(f'https://jsonplaceholder.typicode.com/todos?userId={id}')
-    employee_res = get(f'https://jsonplaceholder.typicode.com/users/{id}')
+    Args:
+        employee_id (int): The ID of the employee.
 
-    todos = todos_res.json()
+    Returns:
+        None
+    """
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print(f"Error: {response.status_code}")
+        return
+
+    todos = response.json()
+    employee_name = todos[0]["username"]
     total_tasks = len(todos)
-    completed_tasks_len = 0
-    completed_tasks_list = []
-    employee_name = employee_res.json()['name']
+    done_tasks = [todo for todo in todos if todo["completed"]]
+    num_done_tasks = len(done_tasks)
 
-    for task in todos:
-        if task['completed'] is True:
-            completed_tasks_len += 1
-            completed_tasks_list.append(task['title'])
+    print(f"Employee {employee_name} is done with tasks({num_done_tasks}/{total_tasks}):")
 
-    print('Employee {} is done with tasks({}/{}):'.
-          format(employee_name,
-                 completed_tasks_len,
-                 total_tasks))
+    for task in done_tasks:
+        print(f"\t {task['title']}")
 
-    for task in completed_tasks_list:
-        print(f'\t {task}')
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    get_todo_list_progress(employee_id)
+    
