b4846e6523b701550c2c1f6e78b07189ee90669a
LanMalJR <lanmaljrlm@gmail.com>
2024-08-14 20:28:17 +0200

    task 0 updated

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 # Fixes typo in php file
 
-exec { 'fix typo in php file':
-  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
-  path    => '/bin'
+exec { 'fix_typo_in_php_file':
+  command => '/bin/sed -i \'s/.phpp/.php/g\' /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin'],  # Include additional common paths
+  unless  => '/bin/grep -q ".phpp" /var/www/html/wp-settings.php',
 }
