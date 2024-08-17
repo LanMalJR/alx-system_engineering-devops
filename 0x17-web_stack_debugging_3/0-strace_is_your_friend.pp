4ea226a231ecd0fa759bb3845012fa7c5ea92543
LanMalJR <lanmaljrlm@gmail.com>
2024-08-17 21:54:42 +0200

    task 0 revert

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,8 +3,3 @@ exec { 'fix_typo_in_php_file':
   path    => ['/bin', '/usr/bin'],
   unless  => '/bin/grep -q ".phpp" /var/www/html/wp-settings.php',
 }
-
-service { 'apache2':
-  ensure => 'running',
-  require => Exec['fix_typo_in_php_file'],
-}
