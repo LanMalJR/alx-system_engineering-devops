d2b246b4c15067b20d9d01435c02da64e5dd1103
LanMalJR <lanmaljrlm@gmail.com>
2024-08-17 21:52:08 +0200

    task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,10 @@
-# Fixes typo in php file
-
 exec { 'fix_typo_in_php_file':
   command => '/bin/sed -i \'s/.phpp/.php/g\' /var/www/html/wp-settings.php',
-  path    => ['/bin', '/usr/bin'],  # Include additional common paths
+  path    => ['/bin', '/usr/bin'],
   unless  => '/bin/grep -q ".phpp" /var/www/html/wp-settings.php',
+}
+
+service { 'apache2':
+  ensure => 'running',
+  require => Exec['fix_typo_in_php_file'],
 }
