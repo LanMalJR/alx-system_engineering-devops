43944007b6835faaef36f2bbaa94b78c6256e653
LanMalJR <lanmaljrlm>
2024-08-18 15:49:08 +0000

    Your commit message

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,5 +0,0 @@
-exec { 'fix_typo_in_php_file':
-  command => '/bin/sed -i \'s/.phpp/.php/g\' /var/www/html/wp-settings.php',
-  path    => ['/bin', '/usr/bin'],
-  unless  => '/bin/grep -q ".phpp" /var/www/html/wp-settings.php',
-}
